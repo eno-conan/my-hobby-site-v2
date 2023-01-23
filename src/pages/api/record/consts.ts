42114b9c6bd140493f1461b238c2b640d4a4d8b6
eno-conan <eno49study@gmail.com>
2023-01-23 19:11:44 +0900

    選択科目にFlutter追加

--- src/pages/api/record/consts.ts
+++ src/pages/api/record/consts.ts
@@ -6,10 +6,11 @@ interface ISubject {
 // 学習単元
 export const SUBJECTS: Array<ISubject> = [
     { value: 999, displayName: '学習分野を選択してください' },
-    { value: 1, displayName: 'React' },
+    { value: 1, displayName: 'React(Next)' },
     { value: 2, displayName: 'Python' },
     { value: 3, displayName: 'AWS' },
     { value: 4, displayName: 'Azure' },
     { value: 5, displayName: 'DB' },
+    { value: 6, displayName: 'Flutter' },
     { value: 0, displayName: 'その他' },
 ]
