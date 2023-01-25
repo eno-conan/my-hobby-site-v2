431f7cbfb5dc1e40b7bd3a24ffcfb29689f70bd2
eno-conan <eno49study@gmail.com>
2023-01-25 22:50:38 +0900

    ヘッダーにtwitterのアイコン設定

---
+++ src/components/ui/twitter-mark/index.tsx
@@ -0,0 +1,28 @@
+import { useTheme } from "next-themes";
+import Image from "next/image";
+
+const TwitterMark = () => {
+  const { theme, systemTheme } = useTheme();
+  const currentTheme = theme === "system" ? systemTheme : theme;
+
+  return (
+    <a
+      href="https://twitter.com/home?lang=ja"
+      aria-label="GitHub"
+      target="_blank"
+      rel="noreferrer"
+      className="relative h-6 w-6 md:h-8 md:w-8 cursor-pointer overflow-hidden"
+    >
+      <Image
+        src={currentTheme === "dark" ? "/images/twitter-mark.png" : "/images/twitter-mark.png"}
+        fill
+        objectFit="cover"
+        alt="github mark"
+        // width={300}
+        // height={300}
+      />
+    </a>
+  );
+};
+
+export default TwitterMark;
